6ddb4c4b0bbe44fb475efdb321d354cd0e125cd6
mahkhalil <khalil.maher91@gmail.com>
2021-06-16 07:19:53 -0400

    adding initial script

---
+++ RegressionScript.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+from sklearn import datasets, linear_model, metrics
+from sklearn.model_selection import train_test_split
+#from pulp import *
+#from lpsolve55 import *
+
+frame = FRAME52387
+
+glucose_levels = frame['Blood_Glucose']
+feature_set = frame[['Regular_Insulin_Dose','Basal_Insulin_Dose','Exercise','Meal_Ingestion']]
+
+X_train, X_test, y_train, y_test = train_test_split(feature_set, glucose_levels, test_size=0.4, random_state=0)
+
+reg = linear_model.LinearRegression()
+reg.fit(X_train, y_train)
+RI = float(reg.coef_[0])
+BI = float(reg.coef_[1])
+EX = float(reg.coef_[2])
+CR = float(reg.coef_[3])
+Int = float(reg.intercept_)
+
+print(reg.coef_)
