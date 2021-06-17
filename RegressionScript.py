7cee03d975a700e510a6f41f983a77ad5c8bedd8
themaherkhalil <khalil.maher91@gmail.com>
2021-06-17 17:20:57 -0400

    Update RegressionScript.py

--- RegressionScript.py
+++ RegressionScript.py
@@ -5,23 +5,4 @@ from sklearn.model_selection import train_test_split
 #from pulp import *
 #from lpsolve55 import *
 
-#here is another comment part 2 
-#here is another comment
-#can you help with using sklearn?
-
-frame = Diabetes_FRAME542300
-
-glucose_levels = frame['Blood_Glucose']
-feature_set = frame[['Regular_Insulin_Dose','Basal_Insulin_Dose','Exercise','Meal_Ingestion']]
-
-X_train, X_test, y_train, y_test = train_test_split(feature_set, glucose_levels, test_size=0.4, random_state=0)
-
-reg = linear_model.LinearRegression()
-reg.fit(X_train, y_train)
-RI = float(reg.coef_[0])
-BI = float(reg.coef_[1])
-EX = float(reg.coef_[2])
-CR = float(reg.coef_[3])
-Int = float(reg.intercept_)
-
-print(reg.coef_)
+# reset to no logic
